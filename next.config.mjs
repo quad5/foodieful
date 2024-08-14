e536f15e10f12e8c40d524a50bbcbdbb0a3fa244
quad5 <thispoc@gmail.com>
2024-08-14 09:17:46 -0700

    added newrelic config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+
 /** @type {import('next').NextConfig} */
+
+// import {nrExternals} from '@newrelic/next/load-externals.js'
+
 const nextConfig = {
-	output: 'standalone',
-	reactStrictMode: false,
+  output: 'standalone',
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
@@ -11,13 +15,28 @@ const nextConfig = {
         pathname: '**',
       },
       {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com',
-          port: '',
-          pathname: '**',
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
       }
     ],
   },
+  // staticPageGenerationTimeout: 600,
+  // experimental: {
+  //   // Without this setting, the Next.js compilation step will routinely
+  //   // try to import files such as `LICENSE` from the `newrelic` module.
+  //   // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
+  //   serverComponentsExternalPackages: ['newrelic']
+  // },
+
+  // // In order for newrelic to effectively instrument a Next.js application,
+  // // the modules that newrelic supports should not be mangled by webpack. Thus,
+  // // we need to "externalize" all of the modules that newrelic supports.
+  // webpack: (config) => {
+  //   nrExternals(config)
+  //   return config
+  // }
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-// import 'newrelic'
+
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
@@ -7,14 +7,24 @@ import Script from 'next/script'
 import { SessionProvider } from 'next-auth/react';
 import theme from '../theme';
 import CssBaseline from '@mui/material/CssBaseline';
+// import newrelic from 'newrelic'
 
 export const metadata = {
   title: 'Foodieful.net',
   description: 'Food truck vendors can list their locations and operating hours.',
 }
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  // if (newrelic.agent.collector.isConnected() === false) {
+  //   await new Promise((resolve) => {
+  //     newrelic.agent.on("connected", resolve)
+  //   })
+  // }
 
+  // const browserTimingHeader = newrelic.getBrowserTimingHeader({
+  //   hasToRemoveScriptWrapper: true,
+  //   allowTransactionlessInjection: true,
+  // })
   return (
     <html lang="en">
       <title>Foodieful</title>
@@ -28,6 +38,20 @@ export default function RootLayout({ children }) {
               <CssBaseline />
               <Menu />
               {children}
+              {/* <Script
+                // We have to set an id for inline scripts.
+                // See https://nextjs.org/docs/app/building-your-application/optimizing/scripts#inline-scripts
+                id="nr-browser-agent"
+                // By setting the strategy to "beforeInteractive" we guarantee that
+                // the script will be added to the document's `head` element.
+                strategy="beforeInteractive"
+                // The body of the script element comes from the async evaluation
+                // of `getInitialProps`. We use the special
+                // `dangerouslySetInnerHTML` to provide that element body. Since
+                // it requires an object with an `__html` property, we pass in an
+                // object literal.
+                dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
+              /> */}
             </ThemeProvider>
           </AppRouterCacheProvider>
         </SessionProvider>
