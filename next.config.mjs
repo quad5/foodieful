c9a21d3233ae157832b0e06ed2e0678cb9ba7eed
quad5 <thispoc@gmail.com>
2024-09-08 17:04:33 -0700

    security settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,67 @@
 /** @type {import('next').NextConfig} */
 
+const cspHeader = `
+    default-src 'self'  https://api.hubspot.com
+    https://forms.hscollectedforms.net
+    https://app.hubspot.com;
+    
+    script-src 'self' 'unsafe-eval' 'unsafe-inline'
+    https://js.hs-scripts.com
+    https://js.usemessages.com
+    https://js.hs-banner.com
+    https://js.hs-analytics.net
+    https://js.hscollectedforms.net
+    https://api.hubspot.com
+    https://udbaa.com;
+
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:  https://forms.hsforms.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    `
+
 const nextConfig = {
   output: 'standalone',
-  reactStrictMode: false,
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "browsing-topics=(), camera=(), geolocation=(); microphone=()",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          }
+        ]
+      },
+
+
+    ]
+  },
   images: {
     remotePatterns: [
       {
--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,18 @@ export default async function RootLayout({ children }) {
 
       <Script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/46942333.js" />
 
+      <Script
+        strategy="lazyOnload"
+        type="text/javascript"
+        id="yllix-script-loader"
+        src="https://udbaa.com/bnr.php?section=General&pub=778719&format=300x250&ga=g" />
+      {/* <noscript><a href="https://yllix.com/publishers/778719" target="_blank">
+        <img src="//ylx-aff.advertica-cdn.com/pub/300x250.png"
+          style="border:none;margin:0;padding:0;vertical-align:baseline;"
+          alt="ylliX - Online Advertising Network" /></a></noscript> */}
+
+
+
       <body>
         <SessionProvider>
           <Suspense fallback={null}>
